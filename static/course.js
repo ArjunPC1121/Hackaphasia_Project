956d12567baf3eab098e3731c5811a21984e144f
Samanvithaa <samanvithaa.rao@gmail.com>
2024-12-05 11:41:01 +0530

    added addl routes in courses page

--- static/course.js
+++ static/course.js
@@ -6,4 +6,23 @@ function c(){
 }
 function java(){
     window.location.href="/java"
+}
+function pythonInt(){
+    window.location.href="/pythonInt"
+}
+function cInt(){
+    window.location.href="/cInt"
+}
+function javaInt(){
+    window.location.href="/javaInt"
+}
+function pythonAdv(){
+    window.location.href="/pythonAdv"
+}
+function cAdv(){
+    window.location.href="/cAdv"
+}
+function javaAdv(){
+    window.location.href="/javaAdv"
+}
 
